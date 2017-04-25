07fac1cf4f0dade980e3c938968558be0e2a465b
Denis Pauk <pauk.denis@gmail.com>
2017-04-25 19:06:00 +0300

    minimal tests

--- cloudify_boto3/rds/tests/test_rds.py
+++ cloudify_boto3/rds/tests/test_rds.py
@@ -16,9 +16,20 @@
 
 from os import path
 import unittest
+from cloudify import mocks as cfy_mocks
+from cloudify.state import current_ctx
 
+from cloudify_boto3.rds.resources import option
 
 class TestRDS(unittest.TestCase):
 
-	def test_RdsSecurityGroup(self):
-		pass
+    def _regen_ctx(self):
+        self.fake_ctx = cfy_mocks.MockCloudifyContext()
+        current_ctx.set(self.fake_ctx)
+
+    def tearDown(self):
+        current_ctx.clear()
+
+    def test_RdsOption(self):
+        self._regen_ctx()
+        option.configure(ctx=self.fake_ctx)
